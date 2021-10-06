31a2aea99fe7a21606fe48270c9dfc1e0e79e73d
Urvashi <urvashidangi.5d@gmail.com>
2021-10-06 20:26:24 +0530

    banner and practise question fix

---
+++ src/main/java/com/aurd/Student/Controller/BannerController.java
@@ -0,0 +1,39 @@
+package com.aurd.Student.Controller;
+
+import com.aurd.Student.Model.Entity.Banners;
+import com.aurd.Student.Model.Response.LatestUpdateResponse;
+import com.aurd.Student.Repository.BannerImageRepository;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.aurd.Service.s3;
+
+@Path("/getBanners")
+public class BannerController {
+    @Inject
+    BannerImageRepository imageRepository;
+
+    @Path("/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+
+
+    public LatestUpdateResponse getImage(@PathParam("id") String instID) throws IOException {
+        ArrayList<Banners> bannerList = (ArrayList<Banners>)
+                imageRepository.list("inst_id",Long.parseLong(instID));
+
+        LatestUpdateResponse response = new LatestUpdateResponse();
+        response.setImageList(bannerList);
+        response.setStatus(true);
+        response.setErrorCode(0);
+        return  response;
+    }
+}
