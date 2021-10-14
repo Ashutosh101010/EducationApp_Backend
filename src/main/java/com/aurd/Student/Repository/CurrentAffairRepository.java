ff97cc031dc06d9deed6a1e25a5163e779cfe3a4
Urvashi <urvashidangi.5d@gmail.com>
2021-10-14 17:37:22 +0530

    blog current affair quiz submit fix

--- src/main/java/com/aurd/Student/Repository/CurrentAffairRepository.java
+++ src/main/java/com/aurd/Student/Repository/CurrentAffairRepository.java
@@ -6,7 +6,9 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.Query;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @ApplicationScoped
@@ -14,6 +16,8 @@ public class CurrentAffairRepository implements PanacheRepository<CurrentAffairM
 
     public ArrayList getCurrentAffairList(GetCurrentAffairRequest request){
         ArrayList<CurrentAffairModel> arrayList = new ArrayList();
+        SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd");
+        Calendar calendar = Calendar.getInstance();
         if(request.getDate().isEmpty()|| request.getDate().equals("")){
 
             String caQuery = "SELECT * from `current_affairs` where  inst_id = ? ORDER BY `time` DESC";
@@ -26,16 +30,29 @@ public class CurrentAffairRepository implements PanacheRepository<CurrentAffairM
 //            arrayList.addAll(list);
 
         }else{
-            String start = request.getDate()+" 00:00:00";
-            String end =  request.getDate()+" 23:59:59";
+            if(request.getDate().equals(formatter.format(calendar.getTime()))){
+                String caQuery = "SELECT * from `current_affairs` where  inst_id = ? ORDER BY `time` DESC";
+                Query currentAffair = getEntityManager().createNativeQuery(caQuery,
+                        CurrentAffairModel.class);
+                currentAffair.setParameter(1,request.getInst_id());
 
-            String caQuery = "SELECT * from `current_affairs` where created_at BETWEEN ? AND ? AND inst_id = ?;";
-            Query currentAffair = getEntityManager().createNativeQuery(caQuery,
-                    CurrentAffairModel.class);
-            currentAffair.setParameter(1,start);
-            currentAffair.setParameter(2,end);
-            currentAffair.setParameter(3,request.getInst_id());
-           arrayList = (ArrayList<CurrentAffairModel>) currentAffair.getResultList();
+                arrayList = (ArrayList<CurrentAffairModel>) currentAffair.getResultList();
+
+            }else{
+                String start = request.getDate()+" 00:00:00";
+                String end =  request.getDate()+" 23:59:59";
+
+                String caQuery = "SELECT * from `current_affairs` where created_at BETWEEN ? AND ? AND inst_id = ?;";
+                Query currentAffair = getEntityManager().createNativeQuery(caQuery,
+                        CurrentAffairModel.class);
+                currentAffair.setParameter(1,start);
+                currentAffair.setParameter(2,end);
+                currentAffair.setParameter(3,request.getInst_id());
+                arrayList = (ArrayList<CurrentAffairModel>) currentAffair.getResultList();
+
+            }
+
+
 
 
         }
